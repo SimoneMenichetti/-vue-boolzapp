523095b1a304572ff4c80dab871b9685063edc1b
simone menichetti <simon3menichettui95@gmail.com>
2024-07-02 17:21:01 +0200

    last refactory

--- BONUS/javascript/mainjavascript.js
+++ BONUS/javascript/mainjavascript.js
@@ -1,20 +1,4 @@
-// MILES 1
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-
-
-// Milestone 2
-// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// Click sul contatto mostra la conversazione del contatto cliccato
-
-
-// Milestone 3
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-
-
-
-
-
+// BOOLZAP BONUSS
 const { createApp } = Vue;
 const { DateTime } = luxon;
 
@@ -279,8 +263,6 @@ createApp({
         this.messageText = '';
         
     },
-
-     
         receiveMessage() {
 
              // Seleziona una frase casuale dalla lista delle frasi meme del nuovo array utilizzando mathfloor per generarla casualmente
